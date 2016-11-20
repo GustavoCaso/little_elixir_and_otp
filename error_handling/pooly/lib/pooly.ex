7fac005c33400879fb0593b875fd380dd9abfcb5
GustavoCaso <gustavocaso@gmail.com>
2016-11-20 18:16:30 +0100

    Implemented queue for Pooly

--- error_handling/pooly/lib/pooly.ex
+++ error_handling/pooly/lib/pooly.ex
@@ -1,19 +1,26 @@
 defmodule Pooly do
   use Application
+  @timeout 5000
 
   def start(_type, _args) do
     pools_config = [
       [
         name: "Pool1",
-        mfa: {SampleWorker, :start_link, []}, size: 5
+        mfa: {SampleWorker, :start_link, []},
+        size: 5,
+        max_overflow: 3
       ],
       [
         name: "Pool2",
-        mfa: {SampleWorker, :start_link, []}, size: 4
+        mfa: {SampleWorker, :start_link, []},
+        size: 4,
+        max_overflow: 0
       ],
       [
         name: "Pool3",
-        mfa: {SampleWorker, :start_link, []}, size: 2
+        mfa: {SampleWorker, :start_link, []},
+        size: 2,
+        max_overflow: 1
       ],
     ]
     start_pool(pools_config)
@@ -23,8 +30,8 @@ defmodule Pooly do
     Pooly.Supervisor.start_link(pools_config)
   end
 
-  def checkout(pool_name) do
-    Pooly.Server.checkout(pool_name)
+  def checkout(pool_name, block \\ true, timeout \\ @timeout) do
+    Pooly.Server.checkout(pool_name, block, timeout)
   end
 
   def checkin(pool_name, worker_pid) do
